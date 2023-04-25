13479b8ca6242278f3299a66e2aea892df4dc689
Prasath007SR <106511003+Prasath007SR@users.noreply.github.com>
2023-04-25 12:05:08 +0530

    commit1

---
+++ snapchatdemo/src/main/java/com/snapchat/demo/controller/SnapchatController.java
@@ -0,0 +1,20 @@
+package com.snapchat.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SnapchatController {
+ @GetMapping("/login")
+ public String getLogin(@RequestParam String username,@RequestParam String password)
+{
+	if(username.equals("admin") && password.equals("12345"))
+	{
+		return "Login Successful";
+	}else
+	{
+		return "Check the user id and password";
+	}
+}
+}
